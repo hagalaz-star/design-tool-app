35e22b1860d8efa0c98c05c8d3bca3913261d9ec
Cyber Nova <saei07@naver.com>
2025-04-29 20:09:01 +0900

    sqlalchemy

---
+++ backend/app/models/design.py
@@ -0,0 +1,4 @@
+from typing import Optional
+from sqlalchemy import Integer, String, ForeignKey`
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
---
+++ backend/app/schemas/design.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+
+class StyleRequest(BaseModel):
+    """
+    프론트엔드에서 백엔드로 보내는 트렌드 스타일 요청 데이터 모델
+    """
